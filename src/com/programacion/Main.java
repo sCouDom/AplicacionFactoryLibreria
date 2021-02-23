8b021c43a7da0775ed618504a9ec28e7a9bb756d
Sergio Counago Dominguez <scounagodominguez@danielcastelao.org>
2021-02-23 10:27:54 +0100

    Main modificado

--- src/com/programacion/Main.java
+++ src/com/programacion/Main.java
@@ -1,8 +1,11 @@
 package com.programacion;
 
+import com.principal.InOut;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        InOut.escribirConsola("hola");
+        InOut.escribirVentana("hola");
     }
 }
