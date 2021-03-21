fdfcf5a67d91f85a81efd9b02a41e601916571b8
Sergio Counago Dominguez <scounagodominguez@danielcastelao.org>
2021-03-21 14:35:18 +0100

    Nuevo codigo main

--- src/com/programacion/Main.java
+++ src/com/programacion/Main.java
@@ -1,11 +1,22 @@
 package com.programacion;
 
-import com.principal.InOut;
+import com.principal.FactoryIn;
+import com.principal.FactoryOut;
+import com.principal.InterfaceComun;
 
 public class Main {
 
     public static void main(String[] args) {
-        InOut.escribirConsola("hola");
-        InOut.escribirVentana("hola");
+        InterfaceComun miSalida,misalida2,misalida3,miEntrada1,miEntrada2;
+        miSalida=FactoryOut.getProductoSalida(1);
+        miSalida.visualizar("Hola");
+        misalida2= FactoryOut.getProductoSalida(2);
+        misalida2.visualizar("Adios");
+        misalida3=FactoryOut.getProductoSalida(3);
+        misalida3.visualizar("Imprimiendo por impresora");
+        miEntrada1= FactoryIn.getProductoEntrada(1);
+        miEntrada1.introducirDatos();
+        miEntrada2=FactoryIn.getProductoEntrada(2);
+        miEntrada2.introducirDatos();
     }
 }
